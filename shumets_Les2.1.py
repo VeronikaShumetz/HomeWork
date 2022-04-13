e6db7d5c40b0d59e2d0e12c93121811d59172569
VeronikaShumetz <veronikashum@gmail.com>
2022-04-14 00:12:48 +0300

    add file

---
+++ shumets_Les2.1.py
@@ -0,0 +1,45 @@
+# 1st task
+address = 'Minsk'
+print(address)
+print(id(address))
+
+address1 = 'Minsk'
+print(address1)
+print(id(address1))
+
+address2 = 'Minsk'
+print(address2)
+print(id(address2))
+
+# 2d task
+
+Nickname = 'weronika'
+print(Nickname)
+print(id(Nickname))
+
+Nickname = 'weronika'
+print(Nickname)
+print(id(Nickname))
+
+# 3d task. Не уверена, что тут сделала корректно(((.
+
+address = 'minsk'
+print(address)
+print(id(address))
+
+address1 = 'MiNsk '
+print(address1)
+print(id(address1))
+
+address2 = ' Minsk'
+print(address2)
+print(id(address2))
+
+
+Nickname = 'Weronika'
+print('Nickname')
+print(id(Nickname))
+
+nickname = 'Weronika'
+print('nickname')
+print(id(nickname))
